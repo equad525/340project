24e253c40b16ddd52e92286b56534f4808527e36
Tyler <tscha316@uwsp.edu>
2016-12-01 01:42:40 -0600

    Add Form Login

---
+++ school/frmLogIn.cs
@@ -0,0 +1,72 @@
+﻿using CourseProject.PD;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseProject.UI
+{
+    public partial class frmLogIn : Form
+    {
+        public frmLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if (isValid())
+            {
+                string trylogin = Main.TryLogin(txtPassword.Text,txtUsername.Text);
+                if (trylogin.Equals("RegistrarStaff"))
+                {
+                    this.Hide();
+                    frmRegistrarStaffHome regStaffForm = new frmRegistrarStaffHome();
+                    regStaffForm.Show();
+                    regStaffForm.Closed += (s, args) => this.Show();
+                    clearLoginForm();
+                }
+                else if (trylogin.Equals("DepartmentStaff"))
+                {
+                    this.Hide();
+                    frmDepartmentStaffHome deptStaffForm = new frmDepartmentStaffHome();
+                    deptStaffForm.Show();
+                    deptStaffForm.Closed += (s, args) => this.Show();
+                    clearLoginForm();
+                }
+                else if (trylogin.Equals("Student"))
+                {
+                    this.Hide();
+                    frmStudentHome studentForm = new frmStudentHome();
+                    studentForm.Show();
+                    studentForm.Closed += (s, args) => this.Show();
+                    clearLoginForm();
+                }
+                else
+                    MessageBox.Show("Incorrect Username or Password");
+            }
+        }
+
+        private bool isValid()
+        {
+            return
+                Validator.IsPresent(txtUsername) &&
+                Validator.IsPresent(txtPassword);
+        }
+        private void clearLoginForm()
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+    }
+}
---
+++ school/frmLogIn.designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CourseProject.UI
+{
+    partial class frmLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(92, 68);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Tag = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(93, 118);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Tag = "Password";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(33, 179);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "LogIn";
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button button1;
+    }
+}
